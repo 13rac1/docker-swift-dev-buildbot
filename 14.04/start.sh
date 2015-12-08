1d9eb95159e253c69b5637be2b0eb09d02b2157d
Brad Erickson <eosrei@gmail.com>
2015-12-07 20:52:14 -0800

    buildbot: Implement test runner for Ubuntu 14.04

---
+++ 14.04/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export SWIFT_BUILD_ROOT=${BUILD}
+
+TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
+
+${SOURCE}/swift/utils/build-script \
+  --preset=buildbot_linux_1404 \
+  install_destdir=${INSTALL} \
+  installable_package=${OUTPUT}/swift-${TIMESTAMP}-ubuntu14.04.tar.gz \
+  2>&1 | tee ${OUTPUT}/build-${TIMESTAMP}.log
