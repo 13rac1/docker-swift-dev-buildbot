32dce84bdc5895cd8a6f899f4ceab365a9075cee
Brad Erickson <eosrei@gmail.com>
2015-12-15 14:45:04 -0800

    ubuntu: Simplify env vars

--- 14.04/build.sh
+++ 14.04/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-export SWIFT_BUILD_ROOT=${BUILD}
-
 TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 
-${SOURCE}/swift/utils/build-script \
+exec /src/swift/utils/build-script \
   --preset=buildbot_linux \
-  install_destdir=${INSTALL} \
-  installable_package=${OUTPUT}/swift-${TIMESTAMP}-ubuntu14.04.tar.gz \
-  2>&1 | tee ${OUTPUT}/build-${TIMESTAMP}.log
+  install_destdir=/install \
+  installable_package=/output/swift-${TIMESTAMP}-ubuntu14.04.tar.gz \
+  2>&1 | tee /output/build-${TIMESTAMP}.log
--- 15.10/build.sh
+++ 15.10/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-export SWIFT_BUILD_ROOT=${BUILD}
-
 TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 
-${SOURCE}/swift/utils/build-script \
+exec /src/swift/utils/build-script \
   --preset=buildbot_linux \
-  install_destdir=${INSTALL} \
-  installable_package=${OUTPUT}/swift-${TIMESTAMP}-ubuntu15.10.tar.gz \
-  2>&1 | tee ${OUTPUT}/build-${TIMESTAMP}.log
+  install_destdir=/install \
+  installable_package=/output/swift-${TIMESTAMP}-ubuntu15.10.tar.gz \
+  2>&1 | tee /output/build-${TIMESTAMP}.log
